5b4ad1969401aebb6517d1c630e125f5598a3498
CowKeyMan <dancauchi1@gmail.com>
2021-12-19 14:32:20 +0200

    Reduce number of commands needed

--- project_02/src/matrix_multiplication.py
+++ project_02/src/matrix_multiplication.py
@@ -1,10 +1,25 @@
+# # running on server
+# from pyspark import SparkContext, SparkConf
+# dataset = "/wrk/group/grp-ddi-2021/datasets/data-2.txt"
+# conf = (
+#     SparkConf()
+#     .setAppName("danicau")
+#     .setMaster("spark://ukko2-10.local.cs.helsinki.fi:7077")
+#     .set("spark.cores.max", "10")
+#     .set("spark.rdd.compress", "true")
+#     .set("spark.broadcast.compress", "true")
+# )
+# sc = SparkContext(conf=conf)
+
+# local testing
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
-
-
-spark = SparkSession.builder.appName("danicau").getOrCreate()
-dataset = 'random_matrix.txt'
-# dataset = 'simple_matrix.txt'
+dataset = 'simple_matrix.txt'
+spark = SparkSession.builder.appName("Spark SQL basic example").getOrCreate()
+conf = SparkConf()
 sc = spark.sparkContext
+
+
 
 
 def line_to_float_list(line: str):
@@ -18,7 +33,6 @@ def pair_rdd_to_same_key(key, pair_rdd):
 
 
 def pair_rdd_to_tuple(key, pair_rdd):
-    # The following may be necessary in case groupByKey does not retain order
     lst = pair_rdd_to_same_key(key, pair_rdd).values().collect()
     return sc.parallelize([(key, lst)])
 
@@ -45,30 +59,26 @@ for i in range(number_of_rows):
     break
 AxAt = sc.union(rows).cache()
 
-# Calcualte A x A.T x A
-AxAtxA = sc.parallelize([])
-for i in range(number_of_rows):
-    row = AxAt.lookup(i)[0]
-    rdd_row = A.map(
-        lambda k_v: (
-            k_v[0],
-            [element * row[k_v[0]] for element in k_v[1]]
-        )
-    )
-    rdd_row = pair_rdd_to_same_key(i, rdd_row)
-    rdd_row = rdd_row.reduceByKey(
-        lambda l1, l2:
-        [a + b for a, b in zip(l1, l2)]
-    )
-    to_write = rdd_row.collect()
-    break
-    AxAtxA = AxAtxA.union(rdd_row)
-
-
+# Calcualte A x A.T x A and write to file
 with open('results.txt', 'w') as f:
-    for element in to_write[0][1]:
-        f.write(f'{element}\n')
+    for i in range(number_of_rows):
+        row = AxAt.lookup(i)[0]
+        rdd_row = A.map(
+            lambda k_v: (
+                k_v[0],
+                [element * row[k_v[0]] for element in k_v[1]]
+            )
+        )
+        rdd_row = rdd_row.reduce(
+            lambda l1, l2: (i, [a + b for a, b in zip(l1[1], l2[1])])
+        )
+        # write row by row
+        # f.write(f'{" ".join([str(elem) for elem in rdd_row[1]])}\n')
+        # write each element to its own line
+        f.write("\n".join([str(elem) for elem in rdd_row[1]]) + '\n')
+        break
+
 
 # checking
-# from checking import check
-# check(dataset, AxAtxA.values().collect())
+# from checking import check, file_to_matrix
+# check(dataset, file_to_matrix('results.txt'))
